e8065eb68e62bdf7e0b2f53142694f57ceaae275
cybroodoo <odoo@cybrosys.com>
2020-08-28 17:49:29 +0530

    test2

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -7,7 +7,7 @@
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose xxxxxxxxxxxxxxxx
+        Long description of module's purpose cccccccccccc
     """,
 
     'author': "My Company",
