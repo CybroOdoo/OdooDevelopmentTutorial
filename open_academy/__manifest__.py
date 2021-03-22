3fe7beec88ce335532c12f14ae49580e18837485
Ajmal Cybro <ajmal@cybrosys.in>
2021-03-22 20:33:50 +0530

    [UPDT] View name changed

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -24,7 +24,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Extra Tools',
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.0.1',
     'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
@@ -36,7 +36,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/open_academy.xml',
+        'views/openacademy.xml',
         'views/partner.xml',
     ],
     # only loaded in demonstration mode
