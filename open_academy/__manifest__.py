fb793bcb02fac85669108a2339e8b1489d146567
Ajmal Cybro <ajmal@cybrosys.in>
2021-04-19 14:46:50 +0530

    [UPDT] Slide update

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -24,7 +24,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Extra Tools',
-    'version': '14.0.1.0.2',
+    'version': '14.0.1.1.3',
     'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
@@ -34,6 +34,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'data/slide_channel_data.xml',
         'views/views.xml',
         'views/templates.xml',
         'views/openacademy.xml',
