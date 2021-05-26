ab33bef4124bc2fee226ff16d4e6fbb97cd811b8
Ajmal Cybro <ajmal@cybrosys.in>
2021-05-26 17:18:44 +0530

    [UPDT] Index Upated

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -23,7 +23,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Extra Tools',
+    'category': 'Tutorials',
     'version': '13.0.1.0.0',
     'license': 'AGPL-3',
 
