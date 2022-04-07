aa254c364e9b0cb916bc49298f724fee96a6f5bc
Ajmal Cybro <ajmal@cybrosys.in>
2022-04-07 10:08:07 +0530

    [ADD] Initial Commit 'open_academy'

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Odoo 14 Development Tutorials",
+    'name': "Odoo 15 Development Tutorials",
 
     'summary': """
-        Odoo 13 And 14 Development Tutorials""",
+        Odoo 15 Development Tutorials""",
 
     'description': """
+        Odoo 15 Development Tutorials,
         Odoo 14 Development Tutorials,
         Odoo 13 Development Tutorials,
         Development Tutorials,
         Odoo Tutorials,
-        Odoo13, Odoo14, odoo Tutorials, odoo learning, odoo13 Tutorials, odoo14 Tutorials, Tutorials,
+        Odoo13, Odoo14, odoo15, odoo Tutorials, odoo learning, odoo13 Tutorials, odoo14 Tutorials, Tutorials,
         Open acadaemy module for managing trainings:
         -Manage student enroll
         -Attendance registration
@@ -29,7 +30,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Tutorials',
-    'version': '14.0.1.4.0',
+    'version': '15.0.1.0.0',
     'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
@@ -40,6 +41,7 @@
     'data': [
         'data/slide_channel_data.xml',
         'data/slide_channel_data_v13.xml',
+        'data/slide_channel_data_v15.xml',
         'security/security.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
